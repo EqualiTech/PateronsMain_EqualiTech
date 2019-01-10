a7b9743bbdf6a660cb9c0bae6ad1bfddf933dd53
Christian B <christianbear1221@gmail.com>
2019-01-09 19:58:51 -0800

    Update picker.js
    _select date and it populates

--- public/admin/picker.js
+++ public/admin/picker.js
@@ -3,8 +3,9 @@
 
 var picker = new Pikaday({
     field: document.getElementById('newDate'),
-    format: 'D MMM YYYY',
+    format: 'MM/DD/YYYY',
     onSelect: function() {
+        changeTodayAndRunWithIt();
         console.log(this.getMoment().format('Do MMMM YYYY'));
     }
 });
