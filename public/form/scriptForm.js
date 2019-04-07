cc5d81f04c1cf2d564e6d05c02d742c196c91d16
Christian B <christianbear1221@gmail.com>
2019-04-06 22:23:32 -0700

    correct?

--- public/form/scriptForm.js
+++ public/form/scriptForm.js
@@ -5,18 +5,10 @@ window.onload = (() =>{
         // sends data to firebase
         sendData(document.getElementById('userName').value);
         document.getElementById('userName').value = "";
-    }); 
-    document.getElementById('dateCentered').addEventListener("keyup", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-            if (event.keyCode === 13) {
-            // Cancel the default action, if needed
-            event.preventDefault();
-            // Trigger the button element with a click
-            //   document.getElementById("myBtn").click();
-            await changeTodayAndRunWithIt();
-            }
+        alert('Saved!');
+            }); 
         });
-      });
+    //   });
     // };
 // });
 
