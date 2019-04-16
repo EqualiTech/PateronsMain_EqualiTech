fe302f09857f7e72e7cfa4da0bb569b5b752ee8e
Christian B <christianbear1221@gmail.com>
2019-04-16 16:11:14 -0700

    working

---
+++ public/admin/firestoreWorking.js
@@ -0,0 +1,63 @@
+//file that deals with creating firestore profiles of paterons
+
+var db2 = firebase.firestore();
+
+function addDataToFirestore(path, data){   
+    db2.collection(path).add(data).
+    catch((error)=>{
+        console.error('error caught', error);
+    });   
+}
+
+function pullDataOdd(path){
+    db2.collection(path).get().
+    then((doc)=>{
+        // console.log('docdata', doc.data());
+        if(doc.exists){
+            console.log('doc', doc);
+        }else{
+            console.log('no doc');
+        }
+    });
+}
+
+function pullDataEven(path){
+    db2.doc(path).get().
+    then((doc)=>{
+        // console.log('docdata', doc.data());
+        if(doc.exists){
+            console.log('doc', doc.data());
+        }else{
+            console.log('no doc');
+        }
+    });
+}
+
+//================================================
+
+var dataMe = []; var firestorePaths = [];
+async function queryData(path){
+    dataMe=[]; firestorePaths=[];
+    await db2.collection(path).get().
+    then(async (snap)=>{
+        snap.forEach(async (doc)=>{
+            // console.log('data', doc.data);
+            await dataMe.push(doc);
+            
+        });
+        for(var i=0; i<dataMe.length; i++){
+            firestorePaths.push(dataMe[i].ref.path);
+        }
+        console.log('firestorePaths', firestorePaths);
+    });
+    
+
+    
+}
+
+//NEXT? - figuring out how to compile all 'fields'
+function pullFields(path){
+
+}
+
+//NEX? - working to get multi layered collections
