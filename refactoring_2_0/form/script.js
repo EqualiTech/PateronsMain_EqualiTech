1572f714edd9f89c1d990214ed289f6abe3f350a
ChristianB <gotrustd@verizon.net>
2020-01-01 23:03:57 -0800

    updating Form - moving on to Admin Table

--- refactoring_2_0/form/script.js
+++ refactoring_2_0/form/script.js
@@ -12,14 +12,22 @@ function onSubmit(){
         var name = document.getElementById('userName').value;
         var userType = document.getElementById('userType').value;
         var reason = document.getElementById('reason').value;
+        var contactMe = contactMeStatus;
         var phone = document.getElementById('contactPhone').value;
         var email = document.getElementById('contactEmail').value;
-        saveWithUpdatedFields(name, userType, reason, phone, email)
+        saveWithUpdatedFields(name, userType, reason, contactMe, phone, email)
     }catch(err){
         alert("somethig failed,", err);
     }
 }
 
+//=============================================
+
+
+
+
+
+var contactMeStatus = "";
 window.onload = ()=>{
     document.getElementById('submitButton').addEventListener('click', ()=>{
         onSubmit();
@@ -36,55 +44,28 @@ window.onload = ()=>{
 
     }); 
 
-    var clickedYes = true;
-    //clicking - YES
-    document.getElementById('yes').addEventListener('click', (e)=>{
-
-        e.preventDefault();
-
-        clickedYes=!clickedYes;
-
-        console.log('clicked', );
-        if(clickedYes){
-            if(document.getElementById('noCheckBox').checked ==true)document.getElementById('noCheckBox').checked =false;
-
+    document.getElementById('yes').addEventListener('click', ()=>{
+        bothClear(()=>{
             document.getElementById('yesCheckBox').checked = true;
-            // document.getElementById('noCheckBox').checked = false;
-            
-            
-        }else{
-            document.getElementById('yesCheckBox').checked = false;
-            // document.getElementById('noCheckBox').checked = false;
-            clickedYes=!clickedYes;
-        }
+            contactMeStatus = "Yes";
+        });
     });
-
-
-    //clicking - NO
-    var clickedNo = true;
-    document.getElementById('no').addEventListener('click', (e)=>{
-
-        e.preventDefault();
-        
-        clickedNo=!clickedNo;
-
-        console.log('clicked', );
-        if(clickedNo){
-            if(document.getElementById('yesCheckBox').checked ==true)document.getElementById('yesCheckBox').checked =false;
-
+    document.getElementById('no').addEventListener('click', ()=>{
+        bothClear(()=>{
             document.getElementById('noCheckBox').checked = true;
-            
-            // document.getElementById('yesCheckBox').checked = false;
+            contactMeStatus = "No";
+        });
 
-            
-        }else{
-            document.getElementById('noCheckBox').checked = false;
-            // document.getElementById('yesCheckBox').checked = false;
-            
-            clickedNo = !clickedNo;
-            
-            // clickedNo=!clickedNo;
-        }
     });
 
 }
+
+
+
+function bothClear(callback){
+    document.getElementById('yesCheckBox').checked = false;
+    document.getElementById('noCheckBox').checked = false; 
+    callback();
+}
+
+
