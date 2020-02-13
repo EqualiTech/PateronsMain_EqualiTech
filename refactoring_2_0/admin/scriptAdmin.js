300f2af95423c9bdaa9741eadfded96e72028c0a
Christian B <christianbear1221@gmail.com>
2020-02-12 19:17:25 -0800

    Old on Desktop side

--- refactoring_2_0/admin/scriptAdmin.js
+++ refactoring_2_0/admin/scriptAdmin.js
@@ -2,3 +2,25 @@
 function makeHTML(){
     
 }
+
+function pullUsersBasedOnTime(date){
+    //function that makes dates into seconds
+
+    var toSecDate = new Date(date);
+    var timeBefore = toSecDate.getTime()/1000;
+
+    timeBefore-=1;
+    console.log('timeBefore', timeBefore);
+
+    var toSecDate2 = new Date(date);
+    timeAfter = (toSecDate2.getTime()/1000)+1;
+    console.log('timeAfter', timeAfter);
+
+    tmp = db.collection('paterons3');
+
+    tmp2 = tmp.where('dateMe', '>', timeBefore ).where('dateMe', '<', timeAfter);
+    tmp2.get().then(async (snap)=>snap.forEach(async (doc)=>{
+        console.log(doc);
+        whereIds.push(doc.id);
+    }));
+}
